ca1f6446fd14da013ea4c4a17ca5b268e980ed0b
Tom Kliethermes <kliethermes.tom@gmail.com>
2016-12-07 17:04:19 -0600

    pause/unpause

--- app/src/main/java/com/quaap/computationaldemonology/GraphicDemonActivity.java
+++ app/src/main/java/com/quaap/computationaldemonology/GraphicDemonActivity.java
@@ -5,21 +5,15 @@ import android.hardware.Sensor;
 import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
-import android.os.Handler;
-import android.os.Message;
+
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.widget.TextView;
 
-import java.util.Timer;
-import java.util.TimerTask;
 
 public class GraphicDemonActivity extends AppCompatActivity implements SensorEventListener {
 
     private SensorManager mSensorManager;
     private Sensor mAccelerometer;
-
-    private Timer timer;
 
 
     @Override
@@ -34,80 +28,43 @@ public class GraphicDemonActivity extends AppCompatActivity implements SensorEve
         mSensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
         mAccelerometer = mSensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
 
+
+        unpause();
+
+
+
+    }
+
+
+    protected void pause() {
+        GraphicDmn dmnview = (GraphicDmn) findViewById(R.id.dmnview);
+        dmnview.pause();
+        mSensorManager.unregisterListener(this);
+    }
+
+    protected void unpause() {
         if (mAccelerometer!=null) {
             mSensorManager.registerListener(this, mAccelerometer, SensorManager.SENSOR_DELAY_NORMAL);
         }
-
-
-       // startTicker();
-
-    }
-
-
-
-//    public Handler mHandler = new Handler() {
-//        public void handleMessage(Message msg) {
-//            TextView mathtxt = (TextView)findViewById(R.id.mathtxt);
-//
-//           // mathtxt.setText(hist);
-//        }
-//    };
-//
-//    private synchronized void startTicker() {
-//        if (timer==null) {
-//            timer = new Timer();
-//            timer.scheduleAtFixedRate(new TimerTask() {
-//                public void run() {
-//
-//                    mHandler.obtainMessage(1).sendToTarget();
-//                }
-//            }, 0, 100);
-//        }
-//    }
-//
-//    private synchronized void stopTicker() {
-//        if (timer!=null) {
-//            timer.cancel();
-//            timer = null;
-//        }
-//    }
-
-    @Override
-    protected void onStart() {
-        super.onStart();
         GraphicDmn dmnview = (GraphicDmn) findViewById(R.id.dmnview);
         dmnview.unpause();
-     //   startTicker();
     }
 
     @Override
-    protected void onStop() {
-        GraphicDmn dmnview = (GraphicDmn) findViewById(R.id.dmnview);
-        dmnview.pause();
-        super.onStop();
-    }
-    /**
-     * Dispatch onPause() to fragments.
-     */
-    @Override
     protected void onPause() {
-    //    stopTicker();
-        mSensorManager.unregisterListener(this);
-        GraphicDmn dmnview = (GraphicDmn) findViewById(R.id.dmnview);
-        dmnview.pause();
-        super.onPause();
+
+        try {
+            pause();
+        } finally {
+            super.onPause();
+        }
     }
 
 
     @Override
     protected void onResume() {
         super.onResume();
-        GraphicDmn dmnview = (GraphicDmn) findViewById(R.id.dmnview);
-        dmnview.unpause();
-        if (mAccelerometer!=null) {
-            mSensorManager.registerListener(this, mAccelerometer, SensorManager.SENSOR_DELAY_NORMAL);
-        }
-     //   startTicker();
+        unpause();
     }
 
     private float mLastX=Float.MAX_VALUE, mLastY, mLastZ;
